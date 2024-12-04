41e8840c1a271e23d4e631e905cf65292eb0895d
Magdalena000000 <magmaj0@centrum.sk>
2024-12-04 23:27:49 +0100

    ide kus

--- src/pages/users.js
+++ src/pages/users.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from '../components/header';
+import Footer from '../components/footer';
 
 const Users = () => {
   return (
     <div>
+      <Header/>
       <h1>Users Page</h1>
       <p>Welcome to the Users page!</p>
+      <Footer/>
     </div>
   );
 };
