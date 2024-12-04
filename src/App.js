35522a3a436cf8ee27a32b6fe78c6e59905d582c
Magdalena000000 <magmaj0@centrum.sk>
2024-12-04 21:07:20 +0100

    nejde barz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import RegistrationForm from './components/form.js'
+import Header from './components/header.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header/>
         <RegistrationForm />
       </header>
     </div>
